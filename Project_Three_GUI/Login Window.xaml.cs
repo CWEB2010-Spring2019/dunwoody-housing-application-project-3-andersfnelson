840889988c3143408a1c8b5d2324654471030a76
Anders  Nelson <nelandf@dunwoody.edu>
2019-03-18 20:27:47 -0500

    login window

---
+++ Project_Three_GUI/Login Window.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project_Three_GUI
+{
+    /// <summary>
+    /// Interaction logic for Login_Window.xaml
+    /// </summary>
+    public partial class Login_Window : Window
+    {
+        string Username;
+        string UserPassword;
+        string CorrectUsername = "home";
+        string CorrectPassword = "1234";
+
+        public Login_Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Username = UsernameBox.Text;
+            UserPassword = PasswordBox.Text;
+            if(Username == CorrectUsername || UserPassword == CorrectPassword)
+            {
+                Console.WriteLine("successful");
+            }
+            else
+            {
+                Console.WriteLine("not successful");
+            }
+        }
+    }
+}
