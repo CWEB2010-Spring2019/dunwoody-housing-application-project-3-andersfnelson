349ebbe577448c663b0d33ade061df9da60529d9
Nelson <nelandf@dunwoody.edu>
2019-03-18 20:42:55 -0500

    checking username

--- Project_Three_GUI/Login Window.xaml.cs
+++ Project_Three_GUI/Login Window.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,28 +20,31 @@ namespace Project_Three_GUI
     /// </summary>
     public partial class Login_Window : Window
     {
-        string Username;
-        string UserPassword;
-        string CorrectUsername = "home";
-        string CorrectPassword = "1234";
+        
 
         public Login_Window()
         {
             InitializeComponent();
         }
+        public string Username;
+        public string UserPassword;
+        public string CorrectUsername = "home";
+        public string CorrectPassword = "1234";
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
             Username = UsernameBox.Text;
             UserPassword = PasswordBox.Text;
-            if(Username == CorrectUsername || UserPassword == CorrectPassword)
+            if (Username == CorrectUsername || UserPassword == CorrectPassword)
             {
-                Console.WriteLine("successful");
+                this.Close();
             }
             else
             {
                 Console.WriteLine("not successful");
             }
+            
         }
+       
     }
 }
