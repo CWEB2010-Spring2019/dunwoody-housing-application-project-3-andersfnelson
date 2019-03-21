6ade4df61223a252a28fecbd97e3284d5a1579c1
Nelson <nelandf@dunwoody.edu>
2019-03-21 12:11:02 -0500

    enforcing floors

--- Project_Three_GUI/AddStudent.xaml.cs
+++ Project_Three_GUI/AddStudent.xaml.cs
@@ -49,18 +49,36 @@ namespace Project_Three_GUI
                     }
                     else
                     {
-                        MessageBox.Show("Athletes can only reside on floors 4, 5, or 6.\n Please make sure you have entered a valid floor number.");
+                        MessageBox.Show("Athletes can only reside on floors 4, 5, or 6.\nPlease make sure you have entered a valid floor number.");
                     }
                 }
+                //floors 7 and 8
                 else if(StudentType.SelectedItem == ScholarshipRecipient)
                 {
-                    App.studentList.Add(new scholarshipRecipient(Convert.ToInt32(floorBox.Text), Convert.ToInt32(rentBox.Text), Convert.ToInt32(idBox.Text), fnameBox.Text, lnameBox.Text, Convert.ToInt32(roomNumBox.Text)));
-                    MessageBox.Show("New scholarship recipient added successfully!");
+                    if (Convert.ToInt32(floorBox.Text) == 7 || Convert.ToInt32(floorBox.Text) == 7)
+                    {
+                        App.studentList.Add(new scholarshipRecipient(Convert.ToInt32(floorBox.Text), Convert.ToInt32(rentBox.Text), Convert.ToInt32(idBox.Text), fnameBox.Text, lnameBox.Text, Convert.ToInt32(roomNumBox.Text)));
+                        MessageBox.Show("New scholarship recipient added successfully!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Scholarship recipients can only reside on floors 7 or 8.\nPlease make sure you have entered a valid floor number.");
+                    }
                 }
+
+                //floors 1/3
                 else if (StudentType.SelectedItem == StudentWorker)
                 {
-                    App.studentList.Add(new worker(Convert.ToInt32(floorBox.Text), Convert.ToInt32(rentBox.Text), Convert.ToInt32(idBox.Text), fnameBox.Text, lnameBox.Text, Convert.ToInt32(roomNumBox.Text)));
-                    MessageBox.Show("New student worker added successfully!");
+                    if (Convert.ToInt32(floorBox.Text) == 1 || Convert.ToInt32(floorBox.Text) == 2 || Convert.ToInt32(floorBox.Text) == 3)
+                    {
+                        App.studentList.Add(new worker(Convert.ToInt32(floorBox.Text), Convert.ToInt32(rentBox.Text), Convert.ToInt32(idBox.Text), fnameBox.Text, lnameBox.Text, Convert.ToInt32(roomNumBox.Text)));
+                        MessageBox.Show("New student worker added successfully!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Workers can only reside on floors 1, 2, or 3.\nPlease make sure you have entered a valid floor number.");
+                    }
+
                 }
             }
             catch
